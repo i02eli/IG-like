188b1350915763f17e1898c1f7b498a89bac96da
Eli <133991967+i02eli@users.noreply.github.com>
2024-08-08 11:25:32 +0800

    Add files via upload

---
+++ pictureList.js
@@ -0,0 +1,59 @@
+const PICTURE_SRC_LIST = [
+  {
+    path: './life.JPG',
+    title: 'title contents',
+    subTitle: 'Proident exercitation magna ullamco sunt.',
+  },
+  {
+    path: './uul.jpg',
+    title: 'title contents',
+    subTitle:
+      'Magna quis adipisicing quis culpa anim cillum reprehenderit amet esse nisi sint qui elit.',
+  },
+  {
+    path: './me2.jpg',
+    title: 'title contents',
+    subTitle: 'Ex sint consectetur velit deserunt.',
+  },
+  {
+    path: './duda.webp',
+    title: 'title contents',
+    subTitle: 'Eiusmod sint consectetur reprehenderit id.',
+  },
+  {
+    path: './ymaa.JPG',
+    title: 'title contents',
+    subTitle:
+      'Ullamco laboris minim consequat et deserunt qui incididunt exercitation.',
+  },
+  {
+    path: './life.JPG',
+    title: 'title contents',
+    subTitle:
+      'Minim nulla quis ut et et sunt adipisicing consectetur mollit non qui magna.',
+  },
+  {
+    path: './uul.jpg',
+    title: 'title contents',
+    subTitle:
+      'Dolor velit nisi proident ipsum ad laborum velit laboris amet adipisicing est reprehenderit sint minim.',
+  },
+  {
+    path: './duda.webp',
+    title: 'title contents',
+    subTitle: 'Sit ut et anim laborum voluptate ea quis Lorem in amet dolore.',
+  },
+  {
+    path: './ymaa.JPG',
+    title: 'title contents',
+    subTitle: 'Proident non sit non irure dolor mollit mollit exercitation ex.',
+  },
+  {
+    path: './me2.jpg',
+    title: 'title contents',
+    subTitle:
+      'Et cillum excepteur cupidatat laborum excepteur aliquip excepteur enim consectetur cillum anim.',
+  },
+]
+
+export default PICTURE_SRC_LIST
